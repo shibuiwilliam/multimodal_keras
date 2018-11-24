fa1dc6e31369787334d6b936b074e40bc81f81a7
shibuiwilliam <shibuiyusuke@gmail.com>
2018-11-24 16:16:54 +0900

    init

---
+++ base_models.py
@@ -0,0 +1,117 @@
+import os
+import random
+import numpy as np
+import pandas as pd
+
+import keras
+from keras import backend as K
+from keras import (Model, Input, optimizers, losses, callbacks)
+from keras.layers import (Activation, Dropout, AlphaDropout, Conv1D, Conv2D,
+                          Reshape, Lambda, GlobalMaxPooling1D, MaxPool2D,
+                          GlobalAveragePooling2D, Dense, MaxPool1D,
+                          GlobalMaxPooling2D, BatchNormalization, Embedding,
+                          Concatenate, Maximum, Add)
+
+
+def numerical_mlp(inputs, output_size):
+    x = Dense(512, activation="relu")(inputs)
+    x = Dropout(0.2)(x)
+    x = Dense(512, activation="relu")(x)
+    x = Dropout(0.2)(x)
+    x = Dense(output_size)(x)
+    return x
+
+
+def text_character_level_cnn(inputs, output_size):
+    x = Embedding(0xffff, 256)(inputs)
+    convs = []
+    for i, p in enumerate([2, 3, 4, 5]):
+        _x = Conv1D(
+            filters=256, kernel_size=p, strides=(p // 2), padding="same")(x)
+        _x = Activation("tanh")(_x)
+        _x = GlobalMaxPooling1D()(_x)
+        convs.append(_x)
+    x = Concatenate()(convs)
+    x = Dense(1024, activation="selu", kernel_initializer="lecun_normal")(x)
+    x = Dropout(0.1)(x)
+    x = Dense(
+        output_size, activation="selu", kernel_initializer="lecun_normal")(x)
+    return x
+
+
+def image_xception(inputs, output_size):
+    cnn = keras.applications.Xception(
+        input_tensor=inputs, include_top=False, weights='imagenet')
+    x = cnn.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(output_size, activation='relu')(x)
+    return x
+
+
+def image_inception_v3(inputs, output_size):
+    cnn = keras.applications.InceptionV3(
+        input_tensor=inputs, include_top=False, weights='imagenet')
+    x = cnn.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(output_size, activation='relu')(x)
+    return x
+
+
+def image_inception_resnet_v2(inputs, output_size):
+    cnn = keras.applications.InceptionResNetV2(
+        input_tensor=inputs, include_top=False, weights='imagenet')
+    x = cnn.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(output_size, activation='relu')(x)
+    return x
+
+
+def sound_cnn(inputs, output_size):
+    def cba(inputs, filters, kernel_size, strides):
+        _x = Conv2D(
+            filters, kernel_size=kernel_size, strides=strides,
+            padding='same')(inputs)
+        _x = BatchNormalization()(_x)
+        _x = Activation("relu")(_x)
+        return _x
+
+    def cba_cluster(x, init_filter=32, init_kernel=(1, 8), init_stride=(1, 2)):
+        _x = cba(
+            x,
+            filters=init_filter,
+            kernel_size=init_kernel,
+            strides=init_stride)
+        _x = cba(
+            _x,
+            filters=init_filter,
+            kernel_size=tuple(reversed(init_kernel)),
+            strides=tuple(reversed(init_stride)))
+        _x = cba(
+            _x,
+            filters=init_filter * 2,
+            kernel_size=tuple(reversed(init_kernel)),
+            strides=tuple(reversed(init_stride)))
+        _x = cba(
+            _x,
+            filters=init_filter * 2,
+            kernel_size=tuple(reversed(init_kernel)),
+            strides=tuple(reversed(init_stride)))
+        return _x
+
+    x_1 = cba_cluster(
+        inputs, init_filter=32, init_kernel=(1, 8), init_stride=(1, 2))
+    x_2 = cba_cluster(
+        inputs, init_filter=32, init_kernel=(1, 16), init_stride=(1, 2))
+    x_3 = cba_cluster(
+        inputs, init_filter=32, init_kernel=(1, 32), init_stride=(1, 2))
+    x_4 = cba_cluster(
+        inputs, init_filter=32, init_kernel=(1, 64), init_stride=(1, 2))
+
+    x = Add()([x_1, x_2, x_3, x_4])
+
+    x = cba(x, filters=128, kernel_size=(1, 16), strides=(1, 2))
+    x = cba(x, filters=128, kernel_size=(16, 1), strides=(2, 1))
+
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(output_size, activation='relu')(x)
+    return x
---
+++ multimodal_model.py
@@ -0,0 +1,236 @@
+import os
+import random
+import numpy as np
+import pandas as pd
+
+import keras
+from keras import backend as K
+from keras import (Model, Input, optimizers, losses)
+from keras.layers import (
+    Activation, Dense, Dropout, AlphaDropout, Conv1D, Conv2D, MaxPool1D,
+    MaxPool2D, GlobalMaxPooling1D, GlobalMaxPooling2D, GlobalAveragePooling2D,
+    Add, Maximum, Concatenate, BatchNormalization, Embedding, Reshape, Lambda)
+
+from multimodal_keras import base_models
+
+
+def merge_layer(layers, output_size):
+    x = Concatenate()(layers)
+    x = Dense(output_size, activation='relu')(x)
+    return x
+
+
+class MultimodalModel():
+    """multimodal model
+    """
+
+    def __init__(self,
+                 inputs_df,
+                 target_column=None,
+                 share_modal_layers=False,
+                 **params):
+        self.inputs_df = inputs_df
+
+        self.target_column = target_column
+
+        self.share_modal_layers = share_modal_layers
+        self.inputs_dict = {}
+        self.feature_extraction_layers = []
+        self.inputs_layers = []
+
+        self.txt_length = params[
+            "txt_length"] if "txt_length" in params else 200
+        self.img_shape = params["img_shape"] if "img_shape" in params else (
+            299, 299, 3)
+        self.snd_freq = params["snd_freq"] if "snd_freq" in params else 128
+        self.snd_time = params["snd_time"] if "snd_time" in params else 1723
+
+        self.num_mlp = params[
+            "num_mlp"] if "num_mlp" in params else base_models.numerical_mlp
+        self.txt_feature_extraction = params[
+            "txt_feature_extraction"] if "txt_feature_extraction" in params else base_models.text_character_level_cnn
+        self.img_feature_extraction = params[
+            "img_feature_extraction"] if "img_feature_extraction" in params else base_models.image_xception
+        self.snd_feature_extraction = params[
+            "snd_feature_extraction"] if "snd_feature_extraction" in params else base_models.sound_cnn
+        self.merge_layer = params[
+            "merge_layer"] if "merge_layer" in params else merge_layer
+
+        self.feature_output_size = params[
+            "feature_output_size"] if "feature_output_size" in params else 256
+
+    def __call__(self):
+        return self.define_feature_extraction_layer()
+
+    def define_feature_extraction_layer(self):
+        num_inputs = []
+        for i, c in enumerate(self.inputs_df.columns):
+            if c.endswith("_num"):
+                num_inputs.append(c)
+            elif c.endswith("_txt"):
+                self._add_txt_layer(c)
+            elif c.endswith("_img"):
+                self._add_img_layer(c)
+            elif c.endswith("_snd"):
+                self._add_snd_layer(c)
+        if i == len(self.inputs_df.columns) - 1 and len(num_inputs) > 0:
+            self._add_num_layer(num_inputs)
+
+        if len(self.feature_extraction_layers) == 0:
+            raise KeyError("No columns for feature extraction")
+        elif len(self.feature_extraction_layers) == 1:
+            return self.feature_extraction_layers[0]
+        else:
+            return self.merge_layer(
+                self.feature_extraction_layers,
+                output_size=self.feature_output_size)
+
+    def _add_layer(self, inputs, x, c):
+        self.feature_extraction_layers.append(x)
+        self.inputs_layers.append(inputs)
+        self.inputs_dict[inputs.name] = c
+
+    def _add_txt_layer(self, c):
+        inputs = Input(shape=(self.txt_length, ))
+        x = self.txt_feature_extraction(
+            inputs, output_size=self.feature_output_size)
+        self._add_layer(inputs, x, c)
+
+    def _add_img_layer(self, c):
+        inputs = Input(shape=self.img_shape)
+        x = self.img_feature_extraction(
+            inputs, output_size=self.feature_output_size)
+        self._add_layer(inputs, x, c)
+
+    def _add_snd_layer(self, c):
+        inputs = Input(shape=(self.snd_freq, self.snd_time, 1))
+        x = self.snd_feature_extraction(
+            inputs, output_size=self.feature_output_size)
+        self._add_layer(inputs, x, c)
+
+    def _add_num_layer(self, num_inputs):
+        inputs = Input(shape=(len(num_inputs), ))
+        x = self.num_mlp(inputs, output_size=self.feature_output_size)
+        self._add_layer(inputs, x, num_inputs)
+
+
+class MultimodalClassifier(MultimodalModel):
+    """multimodal multi-class classifier
+    """
+
+    def __init__(self,
+                 inputs_df,
+                 target_column=None,
+                 num_classes=None,
+                 share_modal_layers=False,
+                 **params):
+        super().__init__(inputs_df, target_column, share_modal_layers,
+                         **params)
+        self._num_classes = num_classes
+        self.num_classes = self._get_target_class_num()
+
+        self.classifier = params[
+            "classifier"] if "classifier" in params else self._classifier
+
+    def __call__(self):
+        x = self.define_feature_extraction_layer()
+        x = self.classifier(x, num_classes=self.num_classes)
+        model = Model(self.inputs_layers, x)
+        return model
+
+    def _get_target_class_num(self):
+        if self.target_column is None:
+            if self._num_classes is None:
+                raise ValueError(
+                    "Either num_classes or target_column should be specified.")
+            else:
+                return self._num_classes
+        else:
+            if self.target_column not in self.inputs_df.columns:
+                raise KeyError("No '{0}' column in input dataframe".format(
+                    self.target_column))
+
+            def _to_categorical():
+                return keras.utils.to_categorical(
+                    self.inputs_df[self.target_column].values)
+
+            _targets = _to_categorical()
+            if self._num_classes is not None:
+                if self._num_classes != len(_targets[0]):
+                    raise ValueError(
+                        "num_classes and classes in target column don't match")
+                else:
+                    return self._num_classes
+            else:
+                return len(_targets[0])
+
+    def _classifier(self, x, num_classes):
+        x = Dropout(0.5)(x)
+        x = Dense(num_classes, activation='softmax')(x)
+        return x
+
+
+class MultimodalRegressor(MultimodalModel):
+    """multimodal regressor
+    """
+
+    def __init__(self,
+                 inputs_df,
+                 target_column=None,
+                 share_modal_layers=False,
+                 **params):
+        super().__init__(inputs_df, target_column, share_modal_layers,
+                         **params)
+
+        self.activation = params[
+            "activation"] if "activation" in params else "linear"
+        self.regressor = params[
+            "regressor"] if "regressor" in params else self._regressor
+
+    def __call__(self):
+        x = self.define_feature_extraction_layer()
+        x = self.regressor(x, activation=self.activation)
+        model = Model(self.inputs_layers, x)
+        return model
+
+    def _validate_target(self):
+        for i in self.inputs_df[self.target_column]:
+            if isinstance(i, float) or isinstance(i, int):
+                raise ValueError("target column should be either int or float")
+
+    def _regressor(self, x, activation):
+        x = Dropout(0.5)(x)
+        x = Dense(1, activation=activation)(x)
+        return x
+
+
+class MultimodalBinaryClassifier(MultimodalModel):
+    """multimodal binary classifier
+    """
+
+    def __init__(self,
+                 inputs_df,
+                 target_column=None,
+                 share_modal_layers=False,
+                 **params):
+        super().__init__(inputs_df, target_column, share_modal_layers,
+                         **params)
+
+        self.binary_classifier = params[
+            "binary_classifier"] if "binary_classifier" in params else self._binary_classifier
+
+    def __call__(self):
+        x = self.define_feature_extraction_layer()
+        x = self.binary_classifier(x)
+        model = Model(self.inputs_layers, x)
+        return model
+
+    def _validate_target(self):
+        for i in self.inputs_df[self.target_column]:
+            if isinstance(i, float) or isinstance(i, int):
+                raise ValueError("target column should be either int or float")
+
+    def _binary_classifier(self, x):
+        x = Dropout(0.5)(x)
+        x = Dense(1, activation="softmax")(x)
+        return x
